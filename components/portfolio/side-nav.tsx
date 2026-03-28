2b74cba413a854532cb2f8ed37909d6aff79d3ce
MertenD <merten.dieckmann@web.de>
2026-03-28 22:12:57 +0100

    Move tabs to component

--- components/portfolio/side-nav.tsx
+++ components/portfolio/side-nav.tsx
@@ -1,11 +1,30 @@
 "use client"
 
-import {navTabsBottom, navTabsTop, Tab, useSideBar} from "@/context/side-bar-context";
+import {useSideBar} from "@/context/side-bar-context";
 import {cn} from "@/lib/utils";
-import {GithubIcon, LinkedinIcon} from "lucide-react";
+import {
+	BotMessageSquareIcon,
+	FolderOpenIcon,
+	GithubIcon,
+	LinkedinIcon,
+	LucideProps,
+	Search,
+	SearchIcon,
+	SettingsIcon
+} from "lucide-react";
 import Link from "next/link";
+import React, {ForwardRefExoticComponent, JSX, RefAttributes} from "react";
+import {FileExplorer} from "@/components/portfolio/sidebar/file-explorer";
 
-export type TabLink = Omit<Tab, "sideBarComponent"> & { href: string }
+export const navTabsTop: Tab[] = [
+	{id: "fileSystem", label: "Project Explorer", icon: FolderOpenIcon, sideBarComponent: <FileExplorer />},
+	{id: "search", label: "Search", icon: SearchIcon, sideBarComponent: <Search />},
+	{id: "chat", label: "Chat", icon: BotMessageSquareIcon, sideBarComponent: <p>Chat</p>},
+]
+
+export const navTabsBottom: Tab[] = [
+	{id: "settings", label: "Settings", icon: SettingsIcon, sideBarComponent: <SettingsIcon />}
+]
 
 export const navTabLinks: TabLink[] = [
 	{id: "github", label: "GitHub", icon: GithubIcon, href: "https://github.com/MertenD"},
@@ -73,3 +92,11 @@ function NavTabLink({ tab }: { tab: TabLink }) {
 	)
 }
 
+export type Tab = {
+	id: string
+	label: string
+	icon: ForwardRefExoticComponent<Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>>
+	sideBarComponent: JSX.Element
+}
+
+export type TabLink = Omit<Tab, "sideBarComponent"> & { href: string }
--- context/side-bar-context.tsx
+++ context/side-bar-context.tsx
@@ -1,26 +1,7 @@
 "use client"
 
-import React, {ForwardRefExoticComponent, JSX, RefAttributes} from "react";
-import {BotMessageSquareIcon, FolderOpenIcon, LucideProps, SearchIcon, SettingsIcon} from "lucide-react";
-import {FileExplorer} from "@/components/portfolio/sidebar/file-explorer";
-import Search from "@/components/portfolio/sidebar/search";
-
-export type Tab = {
-  id: string
-  label: string
-  icon: ForwardRefExoticComponent<Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>>
-  sideBarComponent: JSX.Element
-}
-
-export const navTabsTop: Tab[] = [
-  {id: "fileSystem", label: "Project Explorer", icon: FolderOpenIcon, sideBarComponent: <FileExplorer />},
-  {id: "search", label: "Search", icon: SearchIcon, sideBarComponent: <Search />},
-  {id: "chat", label: "Chat", icon: BotMessageSquareIcon, sideBarComponent: <p>Chat</p>},
-]
-
-export const navTabsBottom: Tab[] = [
-  {id: "settings", label: "Settings", icon: SettingsIcon, sideBarComponent: <p>Settings</p>}
-]
+import React from "react";
+import {navTabsBottom, navTabsTop} from "@/components/portfolio/side-nav";
 
 interface SideBarContextType {
   isOpen: boolean
