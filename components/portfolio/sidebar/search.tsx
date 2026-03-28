4f925ec9524b33c6ee2ad6529336541a85aa4aeb
MertenD <merten.dieckmann@web.de>
2026-03-28 21:42:48 +0100

    Fix search not visible on mobile

--- components/portfolio/sidebar/search.tsx
+++ components/portfolio/sidebar/search.tsx
@@ -2,7 +2,7 @@ import {SearchIcon} from "lucide-react";
 
 export default function Search() {
 
-  return <div className="hidden md:flex items-center bg-popover p-2 rounded border border-border text-muted-foreground text-xs">
+  return <div className="flex items-center bg-popover p-2 rounded border border-border text-muted-foreground text-xs">
     <SearchIcon className="w-3.5 h-3.5 mr-2" />
     <span>Search...</span>
     <span className="ml-4 opacity-50">Ctrl+Shift+A</span>
